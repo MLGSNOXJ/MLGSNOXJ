6faf0758aec9c25af6025b34a16fdd77ec053a24
MLGSNOXJ <59527350+MLGSNOXJ@users.noreply.github.com>
2023-09-07 23:18:29 +0400

    Update and rename 4 to 4.cs

--- 4.cs
+++ 4.cs
@@ -9,7 +9,9 @@ class Program
 
         Console.WriteLine("b=");
         double b = Convert.ToDouble(Console.ReadLine());
+        
+        double c = a * b;
 
-        Console.WriteLine(a + "*" + b + "=" + (a*b));
+        Console.WriteLine(Math.Round(a, 1, MidpointRounding.AwayFromZero) + "*" + Math.Round(b, 1, MidpointRounding.AwayFromZero) + "=" + Math.Round(c, 1, MidpointRounding.AwayFromZero));
     }
 }
