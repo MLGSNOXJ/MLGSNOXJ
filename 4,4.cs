2d309722836828e2d0bc121e7989a64f2b7d9667
MLGSNOXJ <59527350+MLGSNOXJ@users.noreply.github.com>
2023-09-15 12:04:41 +0300

    Update 4,4.cs

--- 4,4.cs
+++ 4,4.cs
@@ -1,14 +1,24 @@
 using System;
+
 class HelloWorld {
   static void Main() {
     int a = Convert.ToInt32(Console.ReadLine());
     int c = Convert.ToInt32(Console.ReadLine());
-    for (int i = 1; i <= a; i++){
-        if(a % i == 0){
-            if(i % c == 0){
-                Console.Write(i + " ");
-            }
+    
+    int sqrtA = (int)Math.Sqrt(a);
+    
+    for (int i = 1; i <= sqrtA; i++){
+      if(a % i == 0){
+        if(i % c == 0){
+          Console.Write(i + " ");
         }
+        
+        int otherFactor = a / i;
+        
+        if(otherFactor != i && otherFactor % c == 0){
+          Console.Write(otherFactor + " ");
+        }
+      }
     }
   }
 }
