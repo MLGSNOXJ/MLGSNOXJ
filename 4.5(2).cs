4719cd83d7758c0b12a32e4d267a919247806d02
MLGSNOXJ <59527350+MLGSNOXJ@users.noreply.github.com>
2023-09-21 21:08:00 +0400

    Create 4.5(2).cs

---
+++ 4.5(2).cs
@@ -0,0 +1,27 @@
+using System;
+
+class Program
+{
+    public static int НОД(int a, int b)
+    {
+        if (a == b)
+        {
+            return a;
+        }
+        else if (a > b)
+        {
+            return НОД(a - b, b);
+        }
+        else
+        {
+            return НОД(a, b - a);
+        }
+    }
+    static void Main()
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int NOD = НОД(a, b);
+        Console.WriteLine(NOD);
+    }
+}
