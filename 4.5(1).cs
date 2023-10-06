47c18e79fe94287ee197711f71b57f3611345966
MLGSNOXJ <59527350+MLGSNOXJ@users.noreply.github.com>
2023-10-06 12:16:37 +0400

    Update 4.5(1).cs

--- 4.5(1).cs
+++ 4.5(1).cs
@@ -277,3 +277,105 @@ class Program
         Console.WriteLine($"Ближайшее предшествующее число к {A} с такой же суммой цифр: {closestNumber}");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using System;
+
+class Program
+{
+    static int Sum(int number)
+    {
+        int sum = 0;
+        while (number != 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+
+    static void SumInRange(int i)
+    {
+            int sum = Sum(i);
+            Console.WriteLine($"Сумма цифр числа {i}: {sum}");
+    }
+
+    static void PrintNumbers(int i, int c)
+    {
+        
+            int sum = Sum(i);
+            if (sum == c)
+            {
+                Console.WriteLine($"Число {i} имеет сумму цифр равную {c}");
+            }
+    }
+
+    static void PrintNNumbers(int i)
+    {
+            int sum = Sum(i);
+            if (sum % 2 != 0)
+            {
+                Console.WriteLine($"Число {i} имеет нечетную сумму цифр");
+            }
+        
+    }
+
+    static int ClosestNumber(int number)
+    {
+        int targetSum = Sum(number);
+        int closestNumber = number - 1;
+        int closestSum = Sum(closestNumber);
+
+        while (closestSum != targetSum)
+        {
+            closestNumber--;
+            closestSum = Sum(closestNumber);
+        }
+
+        return closestNumber;
+    }
+
+    static void Main(string[] args)
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+        int A = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("a) Сумма цифр чисел на отрезке [a, b]:");
+        for (int i = a; i <= b; i++){
+
+        
+        SumInRange(i);
+        }
+        Console.WriteLine("\nb) Числа на отрезке [a, b] с суммой цифр равной C:");
+        for (int i = a; i <= b; i++){
+        
+        PrintNumbers(i, c);
+        }
+        Console.WriteLine("\nc) Числа на отрезке [a, b] с нечетной суммой цифр:");
+
+        for (int i = a; i <= b; i++){
+        PrintNNumbers(i);
+        }
+        Console.WriteLine("\nd) Ближайшее предшествующее число к числу A с такой же суммой цифр:");
+        int closestNumber = ClosestNumber(A);
+        Console.WriteLine($"Ближайшее предшествующее число к {A} с такой же суммой цифр: {closestNumber}");
+    }
+}
