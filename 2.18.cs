88f77334e0cde273ce65a938689e5116660967de
MLGSNOXJ <59527350+MLGSNOXJ@users.noreply.github.com>
2024-02-29 13:07:56 +0300

    Update 2.18.cs

--- 2.18.cs
+++ 2.18.cs
@@ -6,18 +6,15 @@ using System.Collections.Generic;
 [Serializable]
 public abstract class Function
 {
-    // Абстрактный метод, который должны реализовать производные классы для вычисления значения функции в заданной точке
     public abstract double Calculate(double x);
 }
 
 [Serializable]
 public class Line : Function
 {
-    // Параметры прямой: коэффициенты A и B в уравнении y = Ax + B
     public double A { get; set; }
     public double B { get; set; }
 
-    // Реализация метода Calculate для прямой, вычисляющая значение функции для заданного x
     public override double Calculate(double x)
     {
         return A * x + B;
@@ -27,12 +24,10 @@ public class Line : Function
 [Serializable]
 public class Kub : Function
 {
-    // Параметры кубической функции: коэффициенты A, B и C в уравнении y = Ax^2 + Bx + C
     public double A { get; set; }
     public double B { get; set; }
     public double C { get; set; }
 
-    // Реализация метода Calculate для кубической функции, вычисляющая значение функции для заданного x
     public override double Calculate(double x)
     {
         return A * x * x + B * x + C;
@@ -42,11 +37,9 @@ public class Kub : Function
 [Serializable]
 public class Hyperbola : Function
 {
-    // Параметры гиперболы: коэффициенты A и B в уравнении y = A/x + B
     public double A { get; set; }
     public double B { get; set; }
 
-    // Реализация метода Calculate для гиперболы, вычисляющая значение функции для заданного x
     public override double Calculate(double x)
     {
         if (x == 0)
@@ -59,7 +52,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Создаем массив функций, содержащий экземпляры различных классов функций
+        // Сериализация массива функций в бинарный формат и сохранение в файле "functions.bin"
         Function[] functions = new Function[]
         {
             new Line { A = 2, B = 3 },
@@ -67,28 +60,46 @@ class Program
             new Hyperbola { A = 1, B = 2 }
         };
 
+        SerializeFunctions(functions);
+
+        // Десериализация массива функций из файла "functions.bin"
+        Function[] deserializedFunctions = DeserializeFunctions("functions.bin");
+
         // Вычисляем значения функций в заданной точке x
         double x = 5;
         List<double> results = new List<double>();
-        foreach (var function in functions)
+        foreach (var function in deserializedFunctions)
         {
             double result = function.Calculate(x);
             results.Add(result);
         }
 
         // Выводим результаты
-        for (int i = 0; i < functions.Length; i++)
+        for (int i = 0; i < deserializedFunctions.Length; i++)
         {
             Console.WriteLine($"Function {i + 1}: {results[i]}");
         }
 
-        // Сериализация массива функций в бинарный формат и сохранение в файле "functions.bin"
+        Console.ReadLine();
+    }
+
+    static void SerializeFunctions(Function[] functions)
+    {
         using (FileStream fs = new FileStream("functions.bin", FileMode.Create))
         {
             BinaryFormatter formatter = new BinaryFormatter();
             formatter.Serialize(fs, functions);
         }
+    }
 
-        Console.ReadLine();
+    static Function[] DeserializeFunctions(string fileName)
+    {
+        Function[] deserializedFunctions;
+        using (FileStream fs = new FileStream(fileName, FileMode.Open))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            deserializedFunctions = (Function[])formatter.Deserialize(fs);
+        }
+        return deserializedFunctions;
     }
 }
